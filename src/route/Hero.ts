85f4898ee58079d24739bfe321e7c8f1876ddaa3
Alexnet2 <alex.oliveira@viannasempre.com.br>
2021-04-10 11:05:26 -0300

    Mudando o metodo http

--- src/route/Hero.ts
+++ src/route/Hero.ts
@@ -4,7 +4,7 @@ import { searchHeroes, HeroDetails } from 'middlewares/HeroMiddleware'
 import { Hero } from '@types'
 
 const Hero = (app: Express) => {
-    app.post('/search', (req, res, next) => {
+    app.get('/search', (req, res, next) => {
         if (Object.keys(req.query).length == 1) {
             if (req.query.q && req.query.q.length! >= 3) {
                 next()
